fdf56d3cd515ad42ec88af6b38110a29c2c62fd1
lucasimaog6 <2101918@aluno.cotemig.com.br>
2022-10-25 14:16:37 +0000

    add scrollbar & link to buttons

--- js/index.js
+++ js/index.js
@@ -4,10 +4,10 @@ let label_theme = document.getElementById("theme-label");
 
 label_theme.addEventListener("click", () => {
 	if (theme_check.checked == false) {
-				root_element.style.setProperty('--bg', '#abb2bf');
+				root_element.style.setProperty('--bg', '#c9cfdb');
 				root_element.style.setProperty('--fg', '#282828');
 			}else{
 				root_element.style.setProperty('--bg', '#282828');
-				root_element.style.setProperty('--fg', '#abb2bf');
+				root_element.style.setProperty('--fg', '#c9cfdb');
 			}
 })
