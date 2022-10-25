8643aa257c0ba4519cf5da334b169a926510b473
lucasimaog6 <2101918@aluno.cotemig.com.br>
2022-10-25 23:17:09 +0000

    mobiler version

--- js/index.js
+++ js/index.js
@@ -6,8 +6,10 @@ label_theme.addEventListener("click", () => {
 	if (theme_check.checked == false) {
 				root_element.style.setProperty('--bg', '#c9cfdb');
 				root_element.style.setProperty('--fg', '#282828');
+				label_theme.children[0].src = "img/light-mode-icon.svg"
 			}else{
 				root_element.style.setProperty('--bg', '#282828');
 				root_element.style.setProperty('--fg', '#c9cfdb');
+				label_theme.children[0].src = "img/dark-mode-icon.svg"
 			}
 })
